22b6a39deceaebe03295ce18b276db98a0a9faec
zuuhr <zuuhr@users.noreply.github.com>
2021-03-02 17:57:17 +0100

    Creación de html base, instalación de bootstrap y agregado a IndexController index.html
    Se han creado:
    - header.html (presente en toda la navegación)
    - footer.html (presente en toda la navegación)
    - index..html (pagina principal)
    - game.html (página del juego)
    - forum.html (entrada de foro y sus comentarios)
    - navbar.html (presente en toda la navegación, contendrá notificaciones y listas y navegación)
    - profile.html (página de perfil de usuario donde se accederá a datos)
    - customlist.html (página ver la lista de juegos/entradas)

--- gamepost/src/main/java/es/codeurjc/gamepost/controllers/IndexController.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/controllers/IndexController.java
@@ -1,5 +1,13 @@
 package es.codeurjc.gamepost.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class IndexController {
-    
+    @GetMapping(" / ")
+    public String enlace(Model model){
+        return "index";
+    }
 }
