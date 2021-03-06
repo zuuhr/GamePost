e57b09c3e043da0e818903be4af73da633da3bb1
zuuhr <zuuhr@users.noreply.github.com>
2021-03-06 19:34:45 +0100

    Signin e index v0.1
    Barras de navegación y paso de pantallas de logueo y home

--- gamepost/src/main/java/es/codeurjc/gamepost/controllers/IndexController.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/controllers/IndexController.java
@@ -3,11 +3,35 @@ package es.codeurjc.gamepost.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+
 
 @Controller
 public class IndexController {
-    @GetMapping(" / ")
+
+    boolean authenticated;
+    String username;
+
+    
+    @GetMapping("/")
     public String enlace(Model model){
+        // if(!authenticated){
+        //     username = "Log in/Sign in";
+        // } else{
+        //     username = "PC de Alguien";
+        // }
+
+        // model.addAttribute("authenticated", authenticated);
+        // model.addAttribute("username", username);
         return "index";
     }
+    
+    @GetMapping("/signin")
+    public String signin(Model model){
+        return "signin";
+    }
+
+   
+
 }
