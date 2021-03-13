da1fe2f97d1420a5d074d694af3e12ce3093d533
zuuhr <zuuhr@users.noreply.github.com>
2021-03-13 19:55:28 +0100

    Upload game v0.1

--- gamepost/src/main/java/es/codeurjc/gamepost/controllers/GameController.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/controllers/GameController.java
@@ -1,5 +1,7 @@
 package es.codeurjc.gamepost.controllers;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
@@ -84,50 +86,67 @@ public class GameController {
 
         // TODO: Associate this method with the form in the web
         @RequestMapping("/game/submitgame")
-        public String submitGame(Model model, @RequestParam String cover, @RequestParam String name,
-                        @RequestParam List<Genre> genre, @RequestParam int numPlayers, @RequestParam Date publishedDate,
-                        @RequestParam List<Platform> platform, @RequestParam String developer,
-                        @RequestParam String publisher, @RequestParam String synopsis) {
+        public String submitGame(Model model, @RequestParam String titleText,// @RequestParam String cover,
+                        //@RequestParam List<String> genreValues, @RequestParam List<String> platformValues, 
+                        @RequestParam String genreAction,
+                        @RequestParam String playersText, @RequestParam String developerText, 
+                        @RequestParam String releaseText, @RequestParam String publisherText,
+                        @RequestParam String descriptionText) throws ParseException {
 
-                Description d = descriptionRepository.save(new Description(name, genre, numPlayers, publishedDate,
-                                platform, developer, publisher, synopsis));
 
-                gameRepository.save(new Game(cover, d));
+                String cover = "";
 
+                List<Genre> genres = new ArrayList<Genre>();
+                Boolean action = Boolean.parseBoolean(genreAction);
+                if(action){
+                        titleText = "Action";
+                }
+                int numPlayers = Integer.parseInt(playersText);
+                
+                List<Platform> platforms = new ArrayList<Platform>();
+
+                Date releaseDate = new SimpleDateFormat("dd/MM/yyyy").parse(releaseText);
+
+                Description d = new Description(titleText, genres, numPlayers, releaseDate,
+                                platforms, developerText, publisherText, descriptionText);
+
+                Game g = new Game(cover, d);
+
+                gameRepository.save(g);
                 return "submitgame";
         }
 
         @GetMapping("/game/newgame")
-        public String newGame(Model model){
+        public String newGame(Model model) {
                 return "submitgame";
         }
 
-    @GetMapping("/game/{id}")
-    public String getGame(Model model, @PathVariable int id) {
-        Optional<Game> game = gameRepository.findById(id);
-        List<User> users = userRepository.findAll();
+        @GetMapping("/game/{id}")
+        public String getGame(Model model, @PathVariable int id) {
+                Optional<Game> game = gameRepository.findById(id);
+                List<User> users = userRepository.findAll();
 
-        if (game.isPresent()) {
-            model.addAttribute("game", game.get());
-            model.addAttribute("description", game.get().getDescription());
-            ForumEntry fe = new ForumEntry("Hello world", users.get(0), game.get(),
-                    new Content("my firsst content", "url here"));
-            Comment comment = new Comment(users.get(0), new Content(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    "url here"), 0);
-            Comment comment2 = new Comment(users.get(0), new Content(
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                    "url here"), 0);
+                if (game.isPresent()) {
+                        model.addAttribute("game", game.get());
+                        model.addAttribute("description", game.get().getDescription());
+                        ForumEntry fe = new ForumEntry("Hello world", users.get(0), game.get(),
+                                        new Content("my firsst content", "url here"));
+                        Comment comment = new Comment(users.get(0), new Content(
+                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                                        "url here"), 0);
+                        Comment comment2 = new Comment(users.get(0), new Content(
+                                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                                        "url here"), 0);
 
-            fe.addComment(comment);
-            fe.addComment(comment2);
-            forumEntryRepository.save(fe);
-            List<ForumEntry> posts = forumEntryRepository.findAll();
-            model.addAttribute("posts", posts);
-            //TODO: model.addAttribute("user", user);
-            return "game";
-        } else {
-            return "game";
+                        fe.addComment(comment);
+                        fe.addComment(comment2);
+                        forumEntryRepository.save(fe);
+                        List<ForumEntry> posts = forumEntryRepository.findAll();
+                        model.addAttribute("posts", posts);
+                        // TODO: model.addAttribute("user", user);
+                        return "game";
+                } else {
+                        return "game";
+                }
         }
-    }
 }
