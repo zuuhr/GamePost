803c68898d53eb7356f781c375355d3bbd7706b2
Julen <julenjus.creative@gmail.com>
2021-03-16 01:17:53 +0100

    Sort ForumEntry's comments
    This is only a provisional solution. It will be needed to somehow store the info of which level of childness has each comment in order to show it accordingly,

--- gamepost/src/main/java/es/codeurjc/gamepost/repositories/DescriptionRepository.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/repositories/DescriptionRepository.java
@@ -1,13 +1,9 @@
 package es.codeurjc.gamepost.repositories;
 
-import java.util.ArrayList;
-import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.Map.Entry;
-import java.util.stream.Collectors;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
