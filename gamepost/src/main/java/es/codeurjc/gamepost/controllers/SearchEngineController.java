44b13dd888e1c7a31086a35a85793cb65f5d429d
Julen <julenjus.creative@gmail.com>
2021-04-06 17:50:25 +0200

    CustomLists and LatestForumEntries in searchResults page

--- gamepost/src/main/java/es/codeurjc/gamepost/controllers/SearchEngineController.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/controllers/SearchEngineController.java
@@ -6,6 +6,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import es.codeurjc.gamepost.services.CustomListService;
+import es.codeurjc.gamepost.services.ForumEntryService;
 import es.codeurjc.gamepost.services.GameService;
 
 @Controller
@@ -14,10 +16,18 @@ public class SearchEngineController {
     @Autowired
     GameService gameService;
 
+    @Autowired
+    CustomListService customListService;
+
+    @Autowired
+    ForumEntryService forumEntryService;
+
     @RequestMapping("/search")
     public String search(Model model, @RequestParam String searchText){
         
         gameService.search(model, searchText);
+        customListService.showIndex(model);
+        forumEntryService.showIndexLatestForumEntries(model);
         
         return "searchresults";
     }
