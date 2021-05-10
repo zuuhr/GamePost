9b30059eadf7bca73b727c71dd4c5bbd0dc97ca8
zuuhr <zuuhr@users.noreply.github.com>
2021-05-10 18:50:19 +0200

    Hazelcast configuration

--- gamepost/src/main/java/es/codeurjc/gamepost/GamepostApplication.java
+++ gamepost/src/main/java/es/codeurjc/gamepost/GamepostApplication.java
@@ -1,13 +1,33 @@
 package es.codeurjc.gamepost;
 
+import java.util.Collections;
+
+import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 @SpringBootApplication
+@EnableHazelcastHttpSession
 public class GamepostApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GamepostApplication.class, args);
+	}
+
+	@Bean
+	public Config config(){
+		Config config = new Config();
+
+		JoinConfig joinConfig = config.getNetworkConfig().getJoin();
+
+		joinConfig.getMulticastConfig().setEnabled(false);
+		joinConfig.getTcpIpConfig().setEnabled(true).setMembers(Collections.singletonList("127.0.0.1"));
+
+		return config;
 	}
 
 }
