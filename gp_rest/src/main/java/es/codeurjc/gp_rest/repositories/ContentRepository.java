7a7919e01a81b0facbbea8152e797fd4ce1a11ce
Julen <julenjus.creative@gmail.com>
2021-05-10 18:24:30 +0200

    Dockerized API Rest

--- gp_rest/src/main/java/es/codeurjc/gp_rest/repositories/ContentRepository.java
+++
@@ -1,9 +0,0 @@
-package es.codeurjc.gp_rest.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import es.codeurjc.gp_rest.objects.Content;
-
-public interface ContentRepository extends JpaRepository<Content, Integer>{
-    
-}
